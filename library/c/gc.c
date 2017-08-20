6fa5dcae944605507cf6a5df769b026c19d3625a
Philippe Caron <philippe-caron@hotmail.com>
2017-08-20 12:47:56 -0400

    Avancement du gc

--- library/c/gc.c
+++ library/c/gc.c
@@ -25,6 +25,12 @@ struct Mem {
 
 #define INTEGER  0
 #define SPECIAL  1
+// Special values
+   #define FALSE 1
+   #define TRUE  9
+   #define NIL   17
+   #define VOID  33
+   #define UNKN  65
 #define STRING   2
 #define TABLE    3
 #define EMPTY    4
@@ -36,15 +42,109 @@ quad *stack_base;
 quad *mem_size;
 quad *mem_max;
 
-struct Mem gc(quad *stack_ptr, quad *mem_ptr, quad *mem_base) {
+quad *scan;
+quad *copy;
+
+quad heartbreaker = 0xE000000000000000;
+
+quad copydata(quad data)
+{
+  int type = data & 7;
+  quad *point = P(data >> 3);
+  quad *base, *p;
+ 
+  if (*point & heartbreaker) {
+    *copy = (*point << 3) | type;
+    return *(copy++);
+  }
+  switch(type) {
+  case INTEGER:
+    return data;
+  case SPECIAL:
+    return data;
+  case STRING:
+    *copy = *point;
+    *(point++) = heartbreaker | Q(copy);
+    base = copy++;
+    point++;
+    while (1) {
+      *C(copy) = *C(data);
+      copy = P(C(copy) + 1);
+      if (C(point) == 0) break;
+      point = P(C(point) + 1);
+    }
+    return (Q(base) << 3) | STRING;
+  case TABLE:
+    *copy = *point;
+    *(point++) = heartbreaker | Q(copy);
+    base = copy++;
+    // Copy linked list sequence
+    quad *last = copy;
+    while (*point != NIL) {
+      quad a, b;
+      point = P(*point);
+      a = copydata(*point);
+      b = copydata(*(point + 1));
+      *copy = a;
+      *(copy + 1) = b;
+      *last = Q(copy);
+      last = copy + 2;
+      copy = copy + 3;
+      point = point + 2;
+    }
+    *last = NIL;
+    // Copy array sequence
+    point = p;
+    if (*point != nil) {
+      p = P(*point);
+      *(base + 2) = Q(copy);
+      int cap, off, i, first = -1, last;
+      cap = *p >> 3;
+      *(copy++) = *(p++);
+      off = *p >> 3;
+      *(copy++) = *(p++);
+      for (i = 0; i < cap; i ++) {
+	*(copy + i) = *(point + i);
+	if (*(copy + i) != NIL) {
+	  if (first >= 0) first = i;
+	  last = i;
+	}
+      }
+      if ((last - first) * 2 < cap) {
+	// RESIZE !!!
+      }
+      copy = copy + cap;
+    } else *(base + 2) = NIL;
+    return (Q(base) << 3) | TABLE;
+  case EMPTY:
+    return data;
+  case STACK:
+    base = copy;
+    while (*point != VOID)
+    return (Q(base) << 3) | STRING;
+  }
+}
+
+void place_globals(quad *mem_base)
+{
+  
+}
+
+void place_roots(quad *mem_base)
+{
+  
+}
+
+struct Mem gc(quad *stack_ptr, quad *mem_ptr, quad *mem_base)
+{
   struct Mem mem;
   quad *i;
   /*
-  printf("-----------------------------------------\n");
-  for (i = stack_base - 1; i >= stack_ptr; i--) {
+    printf("-----------------------------------------\n");
+    for (i = stack_base - 1; i >= stack_ptr; i--) {
     printf("%p:\t0x%016llX\n", i, *i);
-  }
-  printf("-----------------------------------------\n");
+    }
+    printf("-----------------------------------------\n");
   */
   mem.new_ptr  = mem_ptr;
   mem.new_base = mem_base;
